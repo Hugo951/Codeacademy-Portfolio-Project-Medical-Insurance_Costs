0abe8c2e19c21bd8b44dfdf546680fa70e11392a
Hugo951 <jouko.sivonen@gmail.com>
2021-01-19 19:08:59 +0200

    Update US_medical_insurance_data.py

--- US_medical_insurance_data.py
+++ US_medical_insurance_data.py
@@ -4,9 +4,10 @@ Created on Sat Jan  9 15:32:02 2021
 
 @author: jouko
 """
+# import csv reader
+from csv import reader
 
-import csv
-
+# establish lists for different data
 age = []
 sex = []
 bmi = []
@@ -15,30 +16,104 @@ smoker = []
 region = []
 charges = []
 
-def insurance_data(total_data, column):
-  with open('C:\DATA SCIENCE\PYTHON\PROJECT\PORTFOLIO PROJECT_US Medical Insurance Costs\python-portfolio-project-starter-files\insurance.csv', newline='') as insurance_csv:
-    insurance_reader = csv.DictReader(insurance_csv)
-    for row in insurance_reader:
-        total_data.append(row[column])
-  return total_data
+# read "insurance.csv" file as a list of lists  
+with open('C:\DATA SCIENCE\PYTHON\PROJECT\PORTFOLIO PROJECT_US Medical Insurance Costs\python-portfolio-project-starter-files\insurance.csv', newline='') as insurance_csv:
+  insurance_reader = reader(insurance_csv)
+  list_of_rows = list(insurance_reader)
+  for item in list_of_rows:
+      # skip header information
+      if item[0] == 'age' or item[1] == 'sex' or item[2] == bmi or item[3] == children or item[4] == smoker or item[5] == region or item[6] == charges:
+          pass
+      # add information to named lists
+      else:
+          age.append(item[0])
+          sex.append(item[1])
+          bmi.append(item[2]) 
+          children.append(item[3])
+          smoker.append(item[4])
+          region.append(item[5])
+          charges.append(item[6])
 
-insurance_data(age,'age')
-insurance_data(age,'sex')
-insurance_data(age,'bmi')
-insurance_data(age,'children')
-insurance_data(age,'smoker')
-insurance_data(age,'region')
-insurance_data(age,'charges')
+# will be checked len = number of patient (1338)    
+print(len(age))
+print(len(sex))
+print(len(bmi))
+print(len(children))
+print(len(smoker))
+print(len(region))
+print(charges)
 
-        
-print(age)
+# function for averaging
+def avg_data(lst):
+    sum_of_lst = 0
+    avg_lst = 0
+    for value in lst:
+        sum_of_lst += float(value)
+    avg_lst = sum_of_lst / len(lst)
+    return avg_lst, sum_of_lst
 
-        
-        
-      
+# the averages are stored in the variables
+avg_age = avg_data(age)
+avg_bmi = avg_data(bmi)
+avg_charges = avg_data(charges)
 
+# (39.21, 30.66, 13270.42)
+print(avg_age, avg_bmi, avg_charges)
 
-      
+# comparing insurance costs between smokers and non-smokers  
+# function for combining 2 lists to dict and add the values
+def dict_data(lst1, lst2):
+    res = {}
+    for i in range(len(lst1)):
+        if lst1[i] not in res:
+            res[lst1[i]] = float(lst2[i])
+        else:
+            res[lst1[i]] += float(lst2[i])
+    return res
 
-      
-  
+# dict for smokers and non-smokers insurance costs
+smok = dict_data(smoker, charges)
+
+# count number of smokers and non-smokers
+number_of_smokers = smoker.count('yes')
+number_of_non_smokers = smoker.count('no')
+
+# count smokers and non-smokers average insurance costs
+smokers_avg_charges = smok['yes'] / number_of_smokers
+non_smokers_avg_charges = smok['no'] / number_of_non_smokers
+              
+print(smokers_avg_charges, non_smokers_avg_charges)
+
+# dict for insurance costs by location
+location_data = dict_data(region, charges)
+
+print(location_data)
+
+# count number of people in defferent locations
+number_of_southwest_people = region.count('southwest')
+number_of_southeast_people = region.count('southeast')
+number_of_northwest_people = region.count('northwest')
+number_of_northeast_people = region.count('northeast')
+
+# count average insurance costs by locations
+southwest_avg_charges = location_data['southwest'] / number_of_southwest_people
+southeast_avg_charges = location_data['southeast'] / number_of_southeast_people
+northwest_avg_charges = location_data['northwest'] / number_of_northwest_people
+northeast_avg_charges = location_data['northeast'] / number_of_northeast_people
+ 
+print(southeast_avg_charges, southwest_avg_charges, northeast_avg_charges, northwest_avg_charges)
+
+def dict_data2(children, bmi, charges):
+    children_data = {}
+    for i in range(len(bmi)):
+        children_data[children[i]] = {"Children": children[i], "Bmi": bmi[i], "Charges": charges[i]}
+    return children_data
+
+cd = dict_data2(children, bmi, charges)
+    
+print(cd)
+    
+    
+    
+    
+
